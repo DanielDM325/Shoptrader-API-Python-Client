d26b2ed6cb604144b4be0ebfb3188ff94e471403
Daniël Mizrahi <dmizrahi@live.nl>
2020-03-25 23:03:59 +0100

    Added produts_post_product method

--- shoptrader_api_client.py
+++ shoptrader_api_client.py
@@ -19,3 +19,35 @@ class ShoptraderAPIClient:
             return response.json()
         else:
             return None
+
+    def products_post_product(model, sku, ean, name, quantity, price, purchase_price, status, images, images_alt, height, width, category_id, tax_rate, tax_class_id, meta_title, meta_keyword, meta_description, description, extra_description, intro_description):
+        API_URL = self.API_URL + 'products/'
+        headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
+        data = {
+            'model': model,
+            'sku': str(sku),
+            'ean': str(ean),
+            'name': name,
+            'quantity': str(quantity),
+            'price': str(price),
+            'purchasePrice': str(purchase_price),
+            'images': images,
+            'imagesAlt': images_alt,
+            'height': str(height),
+            'width': str(width),
+            'categoryId': str(category_id),
+            'taxRate': str(tax_rate),
+            'taxClassId': str(tax_class_id),
+            'metaTitle': meta_title,
+            'metaKeyword': meta_keyword,
+            'metaDescription': meta_description,
+            'description': description,
+            'extraDescription': extra_description,
+            'introDescription': intro_description
+        }
+
+        response = requests.post(API_URL, headers=headers, data=data)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
