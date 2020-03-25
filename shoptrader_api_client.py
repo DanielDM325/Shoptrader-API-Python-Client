33d345c962b3cf2346a2264048c640f26f5811b1
Daniël Mizrahi <dmizrahi@live.nl>
2020-03-25 21:06:13 +0100

    Added products_get_product function

--- shoptrader_api_client.py
+++ shoptrader_api_client.py
@@ -9,3 +9,13 @@ class ShoptraderAPIClient:
     def __init__(self, API_URL, API_key):
         self.API_URL = API_URL + self.API_URL
         self.API_key = API_key
+
+    def products_get_product(code_type, product_code):
+        API_URL = self.API_URL + 'products/' + str(product_code)
+        headers = {'accept': 'application/json'}
+        params = {'search', code_type}
+        response = requests.get(API_URL, headers=headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
