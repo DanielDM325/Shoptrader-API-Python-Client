15c89ff9f86104d35f2952d5cde114cb423ac891
Daniël Mizrahi <dmizrahi@live.nl>
2020-03-26 21:23:30 +0100

    Fixed token invocation in all remaining methods

--- shoptrader_api_client.py
+++ shoptrader_api_client.py
@@ -13,15 +13,15 @@ class ShoptraderAPIClient:
     def products_get_product(self, code_type, product_code):
         API_URL = self.API_URL + 'products/' + str(product_code)
         headers = {'accept': 'application/json'}
-        params = {'search', code_type}
-        response = requests.get(API_URL, headers=headers, params=params)
+        params = {'token': self.token['token'], 'search': code_type}
+        response = requests.get(API_URL, params=params, headers=headers)
         if response.status_code == 200:
             return response.json()
         else:
             return None
 
     def products_post_product(self, model, sku, ean, name, quantity, price, purchase_price, status, images, images_alt, height, width, category_id, tax_rate, tax_class_id, meta_title, meta_keyword, meta_description, description, extra_description, intro_description):
-        API_URL = self.API_URL + 'products/'
+        API_URL = self.API_URL + 'products'
         headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
         data = {
             'model': model,
@@ -45,7 +45,7 @@ class ShoptraderAPIClient:
             'extraDescription': extra_description,
             'introDescription': intro_description
         }
-        response = requests.post(API_URL, headers=headers, data=data)
+        response = requests.post(API_URL, params=token, headers=headers, data=data)
         if response.status_code == 200:
             return response.json()
         else:
