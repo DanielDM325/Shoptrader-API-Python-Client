7c2f03bfd56a057b7bb50ebeb26144c81939e7ae
Daniël Mizrahi <dmizrahi@live.nl>
2020-04-11 15:01:21 +0200

    API_URL is now correct, added a possiblity to provide login credentials in case the website is not public

--- shoptrader_api_client.py
+++ shoptrader_api_client.py
@@ -2,24 +2,26 @@ import requests
 
 
 class ShoptraderAPIClient:
-    API_URL = '.shoptrader.com/api/v2/'
+    API_URL = '/api/v2/'
     token = None
     status_code = None
 
-    def __init__(self, API_URL, token):
-        self.API_URL = 'http://' + API_URL + self.API_URL
+    def __init__(self, API_URL, token, credentials=None):
+        self.API_URL = API_URL + self.API_URL
         self.token = {'token': token}
+        self.credentials = credentials
 
     def products_get_product(self, code_type, product_code):
         API_URL = self.API_URL + 'products/' + str(product_code)
         headers = {'accept': 'application/json'}
-        params = {'token': self.token['token'], 'search': code_type}
-        response = requests.get(API_URL, params=params, headers=headers)
+        params = {'search': code_type, 'token': self.token['token']}
+        response = requests.get(API_URL, params=params, headers=headers, auth=self.credentials)
         if response.status_code == 200:
             return response.json()
         else:
             return None
 
+    """
     def products_patch_product(self, product_code):
         API_URL = self.API_URL + 'products/' + str(product_code)
         headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
@@ -37,11 +39,12 @@ class ShoptraderAPIClient:
             'images': images,
             'imagesAlt': images_alt
         }
-        response.patch(API_URL, params=self.token, headers=headers, json=data)
+        response.patch(API_URL, params=self.token, headers=headers, json=data, auth=self.credentials)
         if response.status_code == 200:
             return response.json()
         else:
             return None
+    """
 
     def products_post_product(self, model, sku, ean, name, quantity, price, purchase_price, status, images, images_alt, height, width, category_id, tax_rate, tax_class_id, meta_title, meta_keyword, meta_description, description, extra_description, intro_description):
         API_URL = self.API_URL + 'products'
@@ -69,7 +72,7 @@ class ShoptraderAPIClient:
             'extraDescription': extra_description,
             'introDescription': intro_description
         }
-        response = requests.post(API_URL, params=self.token, headers=headers, json=data)
+        response = requests.post(API_URL, params=self.token, headers=headers, json=data, auth=self.credentials)
         if response.status_code == 200:
             return response.json()
         else:
@@ -78,7 +81,7 @@ class ShoptraderAPIClient:
     def currencies_get_currencies(self):
         API_URL = self.API_URL + 'currencies'
         headers = {'accept': 'aplication/json'}
-        response = requests.get(API_URL, params=self.token, headers=headers)
+        response = requests.get(API_URL, params=self.token, headers=headers, auth=self.credentials)
         if response.status_code == 200:
             return response.json()
         else:
