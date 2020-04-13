a5fbddbf81f61271b3267fa714184e9bcd8410ba
Daniël Mizrahi <dmizrahi@live.nl>
2020-04-13 12:25:29 +0200

    Changed setup name to include username

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="shoptrader_api_client",
+    name="shoptrader-api-client-dmizrahi",
     version="0.1.0",
     author="Daniel Mizrahi",
     author_email="dmizrahi@nodedevelopment.net",
